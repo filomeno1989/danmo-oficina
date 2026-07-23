8ef62b49ca78ed4a5045c93984dd662f47175594
filomeno1989 <filomeno1989@gmail.com>
2026-07-23 14:08:52 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -10,8 +10,8 @@ function renderNavbarOficina(paginaActiva) {
     { id: 'dashboard',     label: 'Dashboard',      href: 'dashboard.html' },
     { id: 'ordens',        label: 'Ordens de Serviço', href: 'ordens.html' },
     { id: 'equipamentos',  label: 'Equipamentos',   href: 'equipamentos.html' },
-    { id: 'colaborador_metricas',  label: 'Metrica',   href: 'colaborador_metricas.html' },
-    { id: 'disponibilidade',  label: 'Disponibilidade',   href: 'disponibilidade.html' },
+   // { id: 'colaborador_metricas',  label: 'Metrica',   href: 'colaborador_metricas.html' },
+   // { id: 'disponibilidade',  label: 'Disponibilidade',   href: 'disponibilidade.html' },
     { id: 'config',        label: '⚙️',         href: 'config.html' },
   ];
 
